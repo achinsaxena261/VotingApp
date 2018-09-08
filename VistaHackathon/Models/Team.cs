5cb40f00d602ff90f6a297623e56bc203d46428f
Ravish Sinha (GlobalLogic India Limited) <v-sinhr@microsoft.com>
2018-09-08 20:41:10 +0530

    Added code for validation adn excel Export

--- VistaHackathon/Models/Team.cs
+++ VistaHackathon/Models/Team.cs
@@ -14,5 +14,41 @@ namespace VistaHackathon.Models
 
 		[DataMember]
 		public string TeamName { get; set; }
+
+		[DataMember]
+		public string TeamIdea { get; set; }
+
+		[DataMember]
+		public int Votes { get; set; }
+
+		[DataMember]
+		public string CompanyName { get; set; }
+	}
+
+	[DataContract]
+	public class FeedbackPage
+	{
+		[DataMember]
+		public string SuccessStatus { get; set; }
+
+		[DataMember]
+		public int MemberId { get; set; }
+
+		[DataMember]
+		public string MemberTeamName { get; set; }
+
+		[DataMember]
+		public string MemberName { get; set; }
+
+		[DataMember]
+		public List<TeamVM> TeamsListForFeedback { get; set; }
+	}
+
+	public enum Status
+	{
+		Valid = 1,
+		InValid,
+		AlreadySubmitted,
+		ValidSuperUser
 	}
 }
